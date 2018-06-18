2eb90d3b94f03e86a08add2fb1e82ab44c4a3f29
Raül Pérez <repejota@gmail.com>
2018-06-19 00:15:41 +0200

    refactor Slugify

--- issue.go
+++ issue.go
@@ -85,13 +85,6 @@ func AssignUserToIssue(organization string, repository string, user *github.User
 	return nil
 }
 
-// Slugify ...
-func Slugify(str string) string {
-	var re = regexp.MustCompile("[^a-z0-9]+")
-	slugifyString := strings.Trim(re.ReplaceAllString(strings.ToLower(str), "-"), "-")
-	return slugifyString
-}
-
 // SlugifyIssue ...
 func SlugifyIssue(issue *github.Issue) string {
 	var re = regexp.MustCompile("[^a-z0-9]+")
